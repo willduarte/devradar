c0f0115315bf5cbd44602e16a7b45b1621b238c9
Willian Duarte de Andrade <willianduarte.me@gmail.com>
2020-01-16 02:04:35 -0300

    adicionando react web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes");
 
 const app = express();
 
 mongoose.connect(
-  'mongodb+srv://omnistack:omnistack@cluster0-55nbc.mongodb.net/week10?retryWrites=true&w=majority',
+  "mongodb+srv://omnistack:omnistack@cluster0-55nbc.mongodb.net/week10?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
+    useCreateIndex: true
   }
 );
 
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
